23346a553c31532c1fdc7de3f39778fc0e5810f8
Ismail Ahmed <isahmed0149@gmail.com>
2023-02-26 19:06:06 -0500

    Finalize UI for search prompt

--- internal/tui/searchprompt.go
+++ internal/tui/searchprompt.go
@@ -3,17 +3,35 @@ package tui
 import (
 	"fmt"
 	"time"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/is-ahmed/command-map/types"
 )
 
 type (
 	errMsg error
 )
+var (
+	titleStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Right = "├"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
+	}()
 
+	infoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return titleStyle.Copy().BorderStyle(b)
+	}()
+)
 type Model struct {
 	textInput textinput.Model
+	viewport viewport.Model
+	ready bool
 	err error	
 }
 
@@ -21,12 +39,11 @@ func InitialModel() Model {
 	ti := textinput.New()
 	ti.Placeholder = ""
 	ti.Focus()
-	ti.CharLimit = 156
-	ti.Width = 20
 
 	return Model{
 		textInput: ti,
 		err:       nil,
+		ready: false,
 	}	
 }
 
@@ -34,7 +51,7 @@ func InitialModel() Model {
 type TickMsg time.Time
 
 func (m Model) Init() tea.Cmd {
-	return textinput.Blink
+	return nil
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -51,17 +68,70 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case errMsg:
 		m.err = msg
 		return m, nil
+	
+	case tea.WindowSizeMsg:
+		headerHeight := lipgloss.Height(m.headerView())
+		footerHeight := lipgloss.Height(m.footerView())
+		verticalMarginHeight := headerHeight + footerHeight
+
+		if !m.ready {
+			// Since this program is using the full size of the viewport we
+			// need to wait until we've received the window dimensions before
+			// we can initialize the viewport. The initial dimensions come in
+			// quickly, though asynchronously, which is why we wait for them
+			// here.
+			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+			m.ready = true
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height - verticalMarginHeight
+		}
+
 	}
 
+	var cmds []tea.Cmd
+	m.viewport.SetContent(m.textInput.Value())
+	// Handle keyboard and mouse events in the viewport
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
 	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd	
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
+
+}
+func (m Model) headerView() string {
+	title := titleStyle.Render("Results")
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+}
+
+func (m Model) footerView() string {
+	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func updateSearchResults(command string) {
+	var commandList []types.Command = types.GetCommands().Commands
+	var commandStrings []string 
+
+	for i:=0; i < len(commandList); i++ {
+		commandStrings = append(commandStrings, commandList[i].Command)
+	}
+
+
+
+
 }
 
 func (m Model) View() string {
-	return fmt.Sprintf(
-		"Search by command\n\n%s\n\n%s",
-		m.textInput.View(),
-		"",
-	) + "\n"
+	return fmt.Sprintf("%s\n%s\n%s", m.textInput.View(), m.headerView(), m.viewport.View())
 }
 
